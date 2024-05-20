fc0ecc91bb47cda98e813e30ced41b467d646fb6
lakondre <andreasadhamaulana@gmail.com>
2024-05-20 22:09:55 +0700

    first commit

---
+++ admin/beranda.php
@@ -0,0 +1,28 @@
+<!DOCTYPE htaml>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Simple Header - CodeWith Random</title>
+        <link rel="stylesheet" type="text/css" href="../assets/css/style.css">       
+	    <link rel="website icon" type="png" href="assets\img\cropped-icon-RSAW-270x270.png">
+    </head>
+    <body>
+        <header class="site-header">
+            <div class="site-identity">
+                <h1><a href="../admin/beranda.html"><img src="../assets/img/logo.png" alt="HTML tutorial" style="width: 258px;height:42px;"></a></h1>
+            </div>
+            <nav class="site-navigation">
+                <ul class="nav">
+                    <li><a href="#">Data Pengguna</a></li>
+                    <li><a href="#">Data Pasien</a></li>
+                    <li><a href="#">Data Riwayat</a></li>
+                    <li><a href="../admin/screening.html">Screening Sistem</a></li>
+                    <li><a href="">Logout</a></li>
+                </ul>
+            </nav>
+        </header>
+
+    </body>
+</html>
